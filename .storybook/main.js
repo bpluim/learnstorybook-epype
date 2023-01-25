21a36f6374ef68a7abb63c169abfd78db9d46977
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2023-01-25 10:53:19 -0700

    adding jest testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,18 @@
+
 module.exports = {
-  "stories": [
+  stories: [
     '../src/Intro.stories.mdx',
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+  features: {
+    interactionsDebugger: true,
+  },
+};
